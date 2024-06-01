fd954abed91443c3432139625b0d896d37efe9f9
Cameron Kauten <cameron.kauten@gmail.com>
2024-06-01 11:54:37 -0700

    feedback form added to mainpage nav

--- public/js/home.js
+++ public/js/home.js
@@ -19,6 +19,17 @@ document.querySelector('.fa-bars').addEventListener('click', function () {
   }
 });
 
+//feedback visibility toggle
+function expandFeedback() {
+  document.getElementById('feedback-expanded').style.height = 'auto';
+  document.getElementById('feedback-expanded').style.visibility = 'visible';
+}
+//feedback collapse toggle
+function collapseFeedback() {
+  document.getElementById('feedback-expanded').style.height = '0em';
+  document.getElementById('feedback-expanded').style.visibility = 'hidden';
+}
+
 // home observer toggle depending on page
 if (document.contains(document.querySelector('.output'))) {
   document.addEventListener('DOMContentLoaded', function () {
@@ -107,52 +118,6 @@ async function generateText() {
     console.error('Error from server');
   }
 }
-
-// typewriter effect for text
-var TxtType = function (el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-};
-
-TxtType.prototype.tick = function () {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  this.txt = fullTxt.substring(0, this.txt.length + 1);
-
-  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-  var that = this;
-  var delta = 100 - Math.random() * 100; // Faster typing effect
-
-  if (this.txt === fullTxt) {
-    return; // Stop once the full text is typed
-  }
-
-  setTimeout(function () {
-    that.tick();
-  }, delta);
-};
-
-window.onload = function () {
-  var elements = document.getElementsByClassName('typewrite');
-  for (var i = 0; i < elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-type');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtType(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  // INJECT CSS
-  var css = document.createElement('style');
-  css.type = 'text/css';
-  css.innerHTML = '.typewrite > .wrap { border-right: 0.08em solid #fff}';
-  document.body.appendChild(css);
-};
 
 // background video function
 (function () {
